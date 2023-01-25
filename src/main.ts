144834d7085bc7be02d2f3f29a6a234989562317
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-01-25 03:26:55 +0100

    fixed small error

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
+import { importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(AppComponent);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));
