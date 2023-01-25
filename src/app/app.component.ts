a7d2b6b88f4f77303ddd5c90e19349777a20e7b7
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-01-25 03:22:48 +0100

    set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
 })
 export class AppComponent {
   title = 'PyramidDisplay';
