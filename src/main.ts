9f89bbf0ee24f909101ba461d8b5ba2248485bf7
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-03-21 21:33:12 +0100

    using translation files to support i18n still using German text in the template files

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
