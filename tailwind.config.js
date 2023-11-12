9682086e89bbb3c7b39e1181c99ed59470fd8f57
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-11-12 02:18:39 +0100

    improved standard display settings view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}']
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1920px',
+      '3xl': '2560px'
+    }
+  }
 }
