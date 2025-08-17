375fd053a9da31b8e0863711cca92881d1da641a
kgabriel-dev <gabrielkleebaum@yahoo.de>
2025-08-17 15:02:20 +0200

    refactor layered method calculator to improve image calculation and update display messages for clarity

--- src/app/services/calculators/layered-method/layered-method-calculator.service.ts
+++ src/app/services/calculators/layered-method/layered-method-calculator.service.ts
@@ -12,28 +12,32 @@ export class LayeredMethodCalculatorService {
     const rectangleAngleDeg = 90 - slopeDeg,
       rectangleAngleRad = rectangleAngleDeg * Math.PI / 180;
 
-    const trapezeTiltedHeight = (canvasWidth / layers) / Math.cos(rectangleAngleRad);
+    // calculate the length of the tilted rectangle
+    // this is the canvas width divided by the number of layers with the slope applied
+    const tiltedHeight = (canvasWidth / layers) / Math.cos(rectangleAngleRad);
     
+    // the width of the rectangle is the same as the canvas height
+    // since the image is shown at a 90-degree angle
     const width = canvasHeight;
 
     // create the image and return it
-    return this.drawImage(trapezeTiltedHeight, width);
+    return this.drawImage(width, tiltedHeight);
   }
 
-  private drawImage(tiltedHeight: number, width: number) {
+  private drawImage(width: number, height: number) {
     const canvas = document.createElement('canvas'),
       ctx = canvas.getContext('2d');
     if(!ctx) return;
 
     canvas.width = width + 3;
-    canvas.height = tiltedHeight + 3;
+    canvas.height = height + 3;
     ctx.fillStyle = 'white'; ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = 'black'; ctx.fillStyle = 'black';
 
     ctx.moveTo(1, 1);
     ctx.lineTo(width + 1, 1);
-    ctx.lineTo(width + 1, tiltedHeight + 1);
-    ctx.lineTo(1, tiltedHeight + 1);
+    ctx.lineTo(width + 1, height + 1);
+    ctx.lineTo(1, height + 1);
     ctx.lineTo(1, 1);
     ctx.stroke(); ctx.fill();
 
